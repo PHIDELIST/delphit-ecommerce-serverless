827e9d26019b880719eeb8e7c7c4c719f401be84
PHIDELIST <phidelisoluoch@gmail.com>
2023-08-21 15:54:00 +0300

    items added to cart

--- frontend-react-ts/src/components/BestSells/BestSales.tsx
+++ frontend-react-ts/src/components/BestSells/BestSales.tsx
@@ -1,73 +1,117 @@
+import React, { useState } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { addToCart } from '../../redux/cartSlice';
+import { RootState } from '../../redux/store';
+import best from '../../assets/veges1.jpg'
 import './BestSales.css'
-const BestSales = () => {
-    const BestSales = [
-        {
-          name: 'Product 1',
-          price: 199,
-         
-        },
-        {
-          name: 'Product 2',
-          price: 299,
-          
-        },
-        {
-          name: 'Product 3',
-          price: 149,
-          
-        },{
-          name: 'Product 4',
-          price: 299,
-          
-        },
-        {
-          name: 'Product 5',
-          price: 299,
-          
-        },
-        {
-          name: 'Product 6',
-          price: 299,
-          
-        },
-        {
-          name: 'Product 7',
-          price: 299,
-          
-        },
-        {
-          name: 'Product 8',
-          price: 299,
-          
-        },
-        {
-          name: 'Product 9',
-          price: 299,
-          
-        },]
-     return(
-        <>
-        <div className='Best-container'>
-          <h1>Daily Best Sales</h1>
-          <div className='Best-Sales-cards'>
-              {BestSales.map((product, index) =>
-                  <div className="card-best" key={index}>
-                      <div className="card-img-best"><div className="img-best"></div></div>
-                      <div className="card-title-best">{product.name}</div>
-                      <div className="card-subtitle-best">Product description. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
-                      <hr className="card-divider-best"></hr>
-                      <div className="card-footer-best">
-                          <div className="card-price-best"><span>$</span>{product.price}</div>
-                          <button className="card-btn-best">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path><path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path></svg>
-                          </button>
-                      </div>
-                  </div>
-                  )}
-          </div>
-        </div>
-        </>
-     )
-}
-export default BestSales;
 
+// Define the product interface
+interface Product {
+    name: string;
+    price: number;
+}
+
+// Define the props types for the component
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+// Props interface for the component
+interface BestSalesProps extends PropsFromRedux {
+    products: Product[];
+}
+
+// Define your dummy data
+const BestSalesData: Product[] = [
+    {
+        name: 'Product 1',
+        price: 199,
+    },
+    {
+        name: 'Product 2',
+        price: 299,
+    },
+    {
+        name: 'Product 3',
+        price: 149,
+    },
+    {
+        name: 'Product 4',
+        price: 299,
+    },
+    {
+        name: 'Product 5',
+        price: 299,
+    },
+    {
+        name: 'Product 6',
+        price: 299,
+    },
+    {
+        name: 'Product 7',
+        price: 299,
+    },
+    {
+        name: 'Product 8',
+        price: 299,
+    },
+    {
+        name: 'Product 9',
+        price: 299,
+    },
+];
+
+const BestSales: React.FC<BestSalesProps> = ({ addToCart }) => {
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+    const handleAddToCart = (product: Product) => {
+        addToCart(product);
+        setSuccessMessage(`${product.name} has been added to your cart.`);
+        // Clear the success message after a delay (e.g., 3 seconds)
+        setTimeout(() => {
+            setSuccessMessage(null);
+        }, 3000);
+    };
+
+    return (
+        <div className='Best-container'>
+            <h1>Daily Best Sales</h1>
+            <div className='Best-Sales-cards'>
+                {BestSalesData.map((product, index) => (
+                    <div className="card-best" key={index}>
+                        <div className="card-img-best">
+                            {/* Placeholder for your image */}
+                            <div className="img-bes">
+                              <img src={best} alt="" />
+                            </div>
+                        </div>
+                        <div className="card-title-best">{product.name}</div>
+                        <div className="card-subtitle-best">
+                            Product description. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </div>
+                        <hr className="card-divider-best" />
+                        <div className="card-footer-best">
+                            <div className="card-price-best">
+                                <span>$</span>{product.price}
+                            </div>
+                            <button
+                                className="card-btn-best"
+                                onClick={() => handleAddToCart(product)}
+                            >
+                                Add to Cart
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            {successMessage && (
+                <div className="success-message">
+                    {successMessage}
+                </div>
+            )}
+        </div>
+    );
+};
+
+// Connect the component to Redux
+const connector = connect(null, { addToCart });
+
+export default connector(BestSales);
--- frontend-react-ts/src/pages/CartPage/CartPage.tsx
+++ frontend-react-ts/src/pages/CartPage/CartPage.tsx
@@ -1,20 +1,50 @@
-import './CartPage.css'
-import Footer from '../../components/Footer/Footer';
+import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { RootState } from '../../redux/store.ts';
 import NavBar1 from '../../components/NavBar1/NavBar1';
 import NavBar2 from '../../components/NavBar2/NavBar2';
-import { useNavigate  } from 'react-router-dom';
-const CartPage = () => {
+import Footer from '../../components/Footer/Footer.tsx';
+import { useNavigate } from 'react-router-dom';
+import itemphoto from '../../assets/veges1.jpg'
+import './CartPage.css'
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const CartPage: React.FC<PropsFromRedux> = ({ cartItems }) => {
     const navigate = useNavigate();
-    return(
+    return (
         <>
-        <NavBar1 />
-        <NavBar2 />
-        <div className='Cart-Main'>
-            <h1>Ooops Nothing is available </h1>
-            <button onClick={() => navigate('/')}>Continue Shopping </button>
-        </div>
-        <Footer />
+            <NavBar1 />
+            <NavBar2 />
+            <div className='Cart-Main'>
+                <h1>Your Cart</h1>
+                {cartItems.length === 0 ? (
+                    <p>Your cart is empty.</p>
+                ) : (
+                    <ul>
+                        {cartItems.map((item, index) => (
+                            <div className='cart-items-container' key={index}>
+                                <p>{item.name}</p>
+                                <p> ${item.price}</p>
+                               <div className='item-img'>
+                                <img src={itemphoto} alt="" />
+                               </div>
+                            </div>
+                        ))}
+                    </ul>
+                )}
+                <button onClick={() => navigate('/')}>Continue Shopping</button>
+            </div>
+            <Footer />
         </>
-    )
-}
-export default CartPage;
+    );
+};
+
+// Map state from the Redux store to component props
+const mapStateToProps = (state: RootState) => ({
+    cartItems: state.cart.cartItems, // Assuming your cart state is under 'cart' slice
+});
+
+// Connect the component to Redux
+const connector = connect(mapStateToProps);
+
+export default connector(CartPage);
