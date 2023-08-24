297a9ca73eedfcbad7e4698cf6163bdff5a2c29f
PHIDELIST <phidelisoluoch@gmail.com>
2023-08-24 15:17:42 +0300

    backend initial commit

--- frontend-react-ts/src/pages/CartPage/CartPage.tsx
+++ frontend-react-ts/src/pages/CartPage/CartPage.tsx
@@ -6,6 +6,7 @@ import NavBar2 from '../../components/NavBar2/NavBar2';
 import Footer from '../../components/Footer/Footer.tsx';
 import { useNavigate } from 'react-router-dom';
 import itemphoto from '../../assets/veges1.jpg'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import './CartPage.css'
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
@@ -15,24 +16,58 @@ const CartPage: React.FC<PropsFromRedux> = ({ cartItems }) => {
         <>
             <NavBar1 />
             <NavBar2 />
-            <div className='Cart-Main'>
-                <h1>Your Cart</h1>
-                {cartItems.length === 0 ? (
-                    <p>Your cart is empty.</p>
-                ) : (
-                    <ul>
-                        {cartItems.map((item, index) => (
-                            <div className='cart-items-container' key={index}>
-                                <p>{item.name}</p>
-                                <p> ${item.price}</p>
-                               <div className='item-img'>
-                                <img src={itemphoto} alt="" />
-                               </div>
-                            </div>
-                        ))}
-                    </ul>
-                )}
-                <button onClick={() => navigate('/')}>Continue Shopping</button>
+            <div className='Cart-Main-container'>  
+                    <div className='Cart-left'>
+                         <h3>Shopping Cart</h3>
+                            <p>There are {cartItems.length} Items in your cart</p>
+                                {cartItems.length === 0 ? (
+                                    <p>Your cart is empty.</p>
+                                ) : (
+                                    <ul>
+                                        {cartItems.map((item, index) => (
+                                            <div className='cart-items-container' key={index}>
+                                                <div className='item-img'>
+                                                    <img src={itemphoto} alt="" />
+                                                </div>
+                                                <div className='cart-item-details'>
+                                                    <p>{item.name}</p>
+                                                    <p> ${item.price}</p>   
+                                                </div>                                        
+                                            </div>
+                                        ))}
+                                        
+                                    </ul>
+                                )}
+                                <button onClick={() => navigate('/')}>Continue Shopping</button>
+                                
+                </div>
+                <div className='Cart-right' >
+                            <h5>Subtotal:</h5>
+                            <p>Discount:</p>
+                            <p>Shipping:</p>
+                            <p>Estimated Tax:</p>
+                            <p>Total:</p>
+                            <button><AddShoppingCartIcon />Checkout </button>
+                            
+                </div>
+                <div className='shipping-coupon'>
+                    <div className='shipping'>
+                        <h5>Calculate Shipping</h5>
+                        <p>Flat rate 5%</p>
+                        <input type="options"  placeholder='search'/>
+                        <div className='county-zip'>
+                        <input type="text" placeholder='State/Country' />
+                        <input type="Post Code /Zip" />
+                        </div>
+                       
+                    </div>
+                    <div className='Coupon' >
+                        <h6>Apply Coupon</h6>
+                        <p>Using a Promo Code?</p>
+                        <input type="text" placeholder='Enter coupon number' />
+                        <button> Apply</button>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
---
+++ frontend-react-ts/src/pages/CategoriesPages/Vegetables/VegetablesPage.tsx
@@ -0,0 +1,11 @@
+import './VegetablesPage.css'
+const VegetablesPage = () => {
+    return(
+        <>
+        <div className='Vegetables-cpontainer-Main' >
+            <h1>Vegetables</h1>
+        </div>
+        </>
+    )
+}
+export default VegetablesPage;
