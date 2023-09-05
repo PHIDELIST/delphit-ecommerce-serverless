569e095bf73c0ccca7d71c10c551c0916a02a3bb
PHIDELIST <phidelisoluoch@gmail.com>
2023-09-05 09:55:38 +0000

    wip

--- frontend-react-ts/src/App.tsx
+++ frontend-react-ts/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Dashboard from './components/Dashboard/Dashboard'
+// import Dashboard from './components/Dashboard/Dashboard'
 import Home from './components/Home/Home'
 import CartPage from './pages/CartPage/CartPage'
 import AboutPage from './pages/AboutPage/AboutPage'
@@ -15,7 +15,7 @@ function App() {
     <NavBar1 />
        <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='admin/dashboard' element={<Dashboard />} />
+        {/* <Route path='admin/dashboard' element={<Dashboard />} /> */}
         <Route path='cart' element={<CartPage />} />
         <Route path='about' element={<AboutPage />} />
         <Route path='setscategory' element={<SetsPage />} />
--- frontend-react-ts/src/components/Dashboard/Dashboard.jsx
+++ frontend-react-ts/src/components/Dashboard/Dashboard.jsx
@@ -17,10 +17,10 @@ import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
-import Chart from './Chart';
-import Deposits from './Deposits';
-import Orders from './Orders';
+import { mainListItems, secondaryListItems } from './listItems.Jsx';
+import Chart from './Chart.jsx';
+import Deposits from './Deposits.jsx';
+import Orders from './Orders.jsx';
 
 function Copyright(props) {
   return (
